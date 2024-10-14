b3c3c1ac7ad911ce7f0bfb03bf0b4304c3c9c192
yuval-shasha <yuvalshasha11@gmail.com>
2024-10-14 16:44:48 +0300

    added support for multiple pages

--- scraper.py
+++ scraper.py
@@ -9,8 +9,15 @@ def scrape(url):
     links = []
     for link in soup.find_all('a'):
         curr_link = link.get('href')
+        if curr_link is None:
+            continue
         curr_link = curr_link.lower()
+        if link.get('href') in links:
+            continue
         # if the link contains at least one of the required words add to the list
         if 'intern' in curr_link or 'student' in curr_link or 'internship' in curr_link:
             links.append(link.get('href'))
+        if 'page' in link.get('href'):
+            links = links + scrape(link.get('href'))
+
     return links
